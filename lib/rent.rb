d4fa2b2833fe23d10728a22860d3ed1295965e2c
Vincent Yang <yusyang686@gmail.com>
2011-11-07 22:17:33 -0500

    TakeAwayTheFormula

--- lib/rent.rb
+++ lib/rent.rb
@@ -3,14 +3,12 @@ class Rent
                   :due_date
                   
     def increase(p)
-      @amount = @amount*(1+p).to_i
     end
 
     def decrease(p)
-      @amount = @amount*(1-p).to_i
     end
     
-    def self.is_paid?()
+    def is_paid?()
      return nil if @amount == 0 
     end
 end
