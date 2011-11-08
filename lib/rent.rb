d3d5c9a10f721833785eb50705e9fe0cb8e21c2f
Glenn Ericksen <glenn@kurioscreative.com>
2011-11-07 21:30:43 -0500

    Create Rent and Utility classes

---
+++ lib/rent.rb
@@ -0,0 +1,18 @@
+class Rent
+  
+  # The total amount due
+  def amount;end
+  
+  # The date rent is due
+  def date_due;end
+  
+  # Apartment identifier
+  def apartment_id;end
+  
+  # Identifies the person receiving payment
+  def landlord_id;end
+  
+  # Identifes the person making payment
+  def tenant_id;end
+  
+end
---
+++ lib/utility.rb
@@ -0,0 +1,9 @@
+class Utility
+
+  # Name of the utility
+  def type;end
+  
+  # Cost of the utility
+  def cost_per_unit;end
+  
+end
