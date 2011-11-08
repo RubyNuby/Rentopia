5de76eaeee47cacb020a76ac68567198c08345da
Vincent Yang <yusyang686@gmail.com>
2011-11-07 21:49:02 -0500

    movedMyRentToLib

--- lib/rent.rb
+++ lib/rent.rb
@@ -1,18 +1,10 @@
 class Rent
-  
-  # The total amount due
-  def amount;end
-  
-  # The date rent is due
-  def date_due;end
-  
-  # Apartment identifier
-  def apartment_id;end
-  
-  # Identifies the person receiving payment
-  def landlord_id;end
-  
-  # Identifes the person making payment
-  def tenant_id;end
-  
+    attr_accessible :amount,
+                    :due_date
+    def increase
+    end
+
+    def decrease
+    end
+  end
 end
--- rent.rb
+++
@@ -1,9 +0,0 @@
-class Rent
-  attr_accessible :amount,
-                  :due_date
-  def increase
-  end
-  
-  def decrease
-  end
-end
