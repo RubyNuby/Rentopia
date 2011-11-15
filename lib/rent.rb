cd078ef814bcf6d1ed98b9296f337e1ae710ec02
Vincent Yang <yusyang686@gmail.com>
2011-11-14 19:00:14 -0500

    refined methods of Rent

--- lib/rent.rb
+++ lib/rent.rb
@@ -1,17 +1,30 @@
 class Rent
     attr_accessor :amount,
                   :due_date,
-				  :apartment_id,
-				  :landlord_id,
-				  :tenant_id
-                  
-    def increase(p)
+				          :apartment_id,
+        				  :landlord_id,
+        				  :tenant_id
+    
+    percentage
+    
+    def increase(percentage)
+      @amount = @amount * (1 + percentage)
     end
 
-    def decrease(p)
+    def decrease(percentage)
+      @amount = @amount * (1 - percentage)
     end
     
     def is_paid?()
      return nil if @amount == 0 
     end
+    
+    def amount(value)
+      @amount = value
+    end
+    
+    def due_date(date)
+      @due_date = date
+    end
+    
 end
