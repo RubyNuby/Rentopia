53ef1b55c65d474e7bc586648f78aa2dcc12d295
Vincent Yang <yusyang686@gmail.com>
2011-11-20 04:03:16 -0500

    refined the increase and decrease methods for Rent

--- lib/rent.rb
+++ lib/rent.rb
@@ -1,21 +1,19 @@
 class Rent
     attr_accessor :amount,
-                  :due_date,
-				          :apartment_id,
-        				  :landlord_id,
-        				  :tenant_id
+                  :due_date
     
     percentage = 0.05
     
     def increase(percentage)
-      @amount = @amount * (1 + percentage)
+      @amount * (1 + percentage)
     end
 
     def decrease(percentage)
-      @amount = @amount * (1 - percentage)
+      @amount * (1 - percentage)
     end
     
     def is_paid?()
-     return nil if @amount == 0 
+      return nil if @amount == 0 
     end
+    
 end
